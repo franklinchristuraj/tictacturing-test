062aa3896454e8679891b082d5c2f0fbdfacac97
Franklin Christuraj <christuraj.anto@gmail.com>
2019-07-06 19:23:45 +0200

    Bug fixes

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,18 +1,26 @@
 import React, {Component} from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+
 
 class Template extends Component {
 
   render() {
     return (
+      <MuiThemeProvider>
       <div>
         <header>
           <h1>TicTacTuring</h1>
+          <Button
+          onTouchTap={()=>{console.log('hello, I work')}}
+          />
         </header>
         <main>
           {this.props.children}
         </main>
-
       </div>
+      </MuiThemeProvider>
     )
   }
 }
